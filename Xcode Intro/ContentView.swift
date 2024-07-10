13ec0d7267ec64ac65d61a9bc3c529806035d36d
Rishpan <34177557+Rishpan@users.noreply.github.com>
2024-07-10 11:11:08 -0700

    Initial Commit

---
+++ Xcode Intro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xcode Intro
+//
+//  Created by Rishab Pangal on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xcode Intro/Xcode_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_IntroApp.swift
+//  Xcode Intro
+//
+//  Created by Rishab Pangal on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
